2235b5eed807b68296bf2ad7a66af434fb850588
danie <greenhalghdan@gmail.com>
2023-06-13 23:46:25 +0100

    random password generator

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    # Password Generator Project
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+    password_list += [random.choice(numbers) for char in range(nr_numbers)]
+    password_list += [random.choice(symbols) for char in range(nr_letters)]
+    password_list += [random.choice(letters) for char in range(nr_symbols)]
+
+    # for char in range(nr_letters):
+    #     password_list.append(random.choice(letters))
+    #
+    # for char in range(nr_symbols):
+    #     password_list += random.choice(symbols)
+    #
+    # for char in range(nr_numbers):
+    #     password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_input.insert(0, password)
+    #password_input.grid(column=2, row=4, columnspan=1)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_entry():
-    if len(username_input.get()) > 0 and len(website_input.get()) > 0 and len(password_input.get()) > 0:
+    if len(username_input.get()) > 0 or len(website_input.get()) > 0 or len(password_input.get()) > 0:
         is_ok = messagebox.askokcancel(title=website_input.get(), message=f"These are the deatils entered: \nemail: {username_input.get()} \nPassword: {password_input.get()} \n is it ok to save?")
         if is_ok:
             with open("data.txt", "a") as passworddb:
@@ -51,7 +85,7 @@ password_label = Label(text="Password:")
 password_label.grid(column=1, row=4)
 
 password_input = Entry(width=21)
-password_input.grid(column=2, row=4, columnspan=1, )
+password_input.grid(column=2, row=4, columnspan=1)
 
 # genorate password
 
